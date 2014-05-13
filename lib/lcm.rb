f6b2cc89b56b8e6dae8178050200ca18651b438b
Jake Mays <quaunaut@gmail.com>
2014-05-13 05:42:30 -0500

    Got lcm working using gcd.

--- lib/lcm.rb
+++ lib/lcm.rb
@@ -1,7 +1,29 @@
 class LCMService
-  
+
   def lcm(a, b)
-      raise "Not Implemented"
+    if a.nil? || b.nil?
+      return nil
+    end
+    if (a < 1) || (b < 1)
+      return nil
+    end
+    return (a / gcd(a,b)) * b
   end
-  
+
+  private
+
+  def gcd(a, b)
+    if a < b
+      a, b = b, a
+    end
+    loop do
+      remainder = a % b
+      if remainder == 0
+        return b
+      else
+        a, b = b, remainder
+      end
+    end
+  end
+
 end
