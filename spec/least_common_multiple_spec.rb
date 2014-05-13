a658c315b30447204925fada8baea5992405b7fe
Jake Mays <quaunaut@gmail.com>
2014-05-13 06:34:57 -0500

    Fixed spelling error.

--- spec/least_common_multiple_spec.rb
+++ spec/least_common_multiple_spec.rb
@@ -12,8 +12,8 @@ describe "Least Common Multiple" do
   before(:all) do
     @service = LCMService.new
   end
-  
-  it "should not calculate LCM for a zero on nil argument" do
+
+  it "should not calculate LCM for a zero or nil argument" do
     @service.lcm(0,4).should be_nil
     @service.lcm(2, nil).should be_nil
   end
@@ -25,5 +25,5 @@ describe "Least Common Multiple" do
   it "should calculate LCM of 4 and 6" do
     @service.lcm(4,6).should == 12
   end
-  
+
 end
