69498a301fdb5580734c423590c662e377b8be87
Jake Mays <quaunaut@gmail.com>
2014-05-13 06:32:00 -0500

    Added solution to eratosthenes class.

--- lib/eratosthenes.rb
+++ lib/eratosthenes.rb
@@ -1,10 +1,21 @@
 class Eratosthenes
   def initialize(max_number)
-    @max = max_number
+    if max_number.is_a? Integer
+      @max = max_number
+    else
+      fail "#{max_number} is not an Integer."
+    end
   end
-  
+
   def primes
-      raise "Not Implemented"
+    sieve = (0..@max).to_a
+    sieve[0] = sieve[1] = nil
+    sieve.each do |part|
+      next unless part
+      break if part * part > @max
+      (part*part).step(@max, part) { |match| sieve[match] = nil }
+    end
+    sieve.compact
   end
-  
+
 end
